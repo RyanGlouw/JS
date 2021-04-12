92a6e02ac584e650188d08b06fe50b4065ba5645
ryantodd <ryantodd1176@gmail.com>
2021-04-13 00:15:44 +0300

    2of3tasks

--- homeworks/lesson_3/js/massives.js
+++ homeworks/lesson_3/js/massives.js
@@ -9,24 +9,49 @@
 //     ]
 // Увеличить значение каждого элемента массива на 10
 
+let arr = [
+    [23, 56, 75, -90, 123],
+    [17, 0, -6429, -122],
+    [19, 86, 55, -3, 900, 0, 0],
+    [4, 9, -2]
+]
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        arr[i][j] += 10;
+    }
+}
+
+console.log(arr);
+
+
+
+
 // 2. Создать массив из целых чисел, заполнить массив рандомными значениями. Размер массива - 7.
 // Поменять элементы с максимальным и минимальным значениями местами.
+
+let randomNum = Math.floor(Math.random() * 100) + 1;
+let arr = []
+for (let i = 1; i <= 7; i++) {
+    arr.unshift(randomNum);
+}
+console.log(arr);
 
 // 3. Создать массив из целых чисел.
 // Отрицательные элементы массива скопировать в новый массив.
 
-let numbers = [1,-2,3,-4,5];
+let numbers = [1, -2, 3, -4, 5, -6];
 let minArr = [];
-for (let i = 0; i < numbers.length; i ++) {
- if(numbers[i] <= 0){
-    let num = numbers.splice(i,1);
-    minArr.push(num);
-}
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] <= 0) {
+        let num = numbers.splice(i, 1);
+        minArr.push(num);
+    }
 }
 console.log(minArr);
 console.log(numbers); // более сложный вариант
 
-let numbers = [1,-2,-3,4,5];
-let minArr = numbers.splice(1,2);
+let numbers = [1, -2, -3, 4, 5];
+let minArr = numbers.splice(1, 2);
 console.log(minArr);
 console.log(numbers); // более простой и не практичный
