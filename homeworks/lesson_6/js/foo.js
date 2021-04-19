dceedb611f9df521acef194ea7ca50a16b92794d
ryantodd <ryantodd1176@gmail.com>
2021-04-20 00:24:02 +0300

    hwDone

--- homeworks/lesson_6/js/foo.js
+++ homeworks/lesson_6/js/foo.js
@@ -3,6 +3,44 @@
 // Например, слово "дом" встречается в строке "дом домик домой одомашненный" 
 // 4 раза.
 
+// function enterStr(word, str){
+// let count = 0;
+// str.split('');
+// for (let i = 0; i < str.length; i++){
+//     if (str[i] === word){
+//         count++;
+//     }
+//     return count;
+// }
+// }
+
+// let one = 'дом';
+// let two = 'дом домик домой одомашненный';
+// console.log(enterStr(one,two));
+
+function getOccurrenceCount(string, stringToSearch) {
+    let count = 0
+     let index = 0
+     let foundedIndex = 0
+     
+     while (foundedIndex >= 0) {
+       foundedIndex = string.indexOf(stringToSearch, index)
+       if (foundedIndex >= 0) {
+        count++
+         index = foundedIndex + stringToSearch.length
+       }
+     }
+     
+     return count
+   }
+   
+   let stringToSearch = 'дом'
+   let string = 'дом домик домой одомашненный'
+   
+   console.log(
+    getOccurrenceCount(string, stringToSearch)
+   )
+
 // 2. Написать функцию, которая принимает на вход строку и возвращает true,
 //  если функция является полиндромом и false, если нет.
 // Палиндром — это число, буквосочетание, слово или текст, которые одинаково 
